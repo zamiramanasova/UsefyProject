407c0674f8c0a61dee5b8ab255dc719a856de98e
Zamira <keldibaevaz@gmail.com>
2025-09-14 21:02:34 +0300

    Initial commit

---
+++ src/main/java/com/example/usefy/UsefyApplication.java
@@ -0,0 +1,14 @@
+package com.example.usefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsefyApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/com/example/usefy/UsefyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsefyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
