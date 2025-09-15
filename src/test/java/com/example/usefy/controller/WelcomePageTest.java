8664250668719918cc532c384d682298229f1275
Zamira <keldibaevaz@gmail.com>
2025-09-15 21:16:03 +0300

    15/09/2025

---
+++ src/test/java/com/example/usefy/controller/WelcomePageTest.java
@@ -0,0 +1,18 @@
+package com.example.usefy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.ui.ModelMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WelcomePageTest {
+
+    @Test
+    void printWelcome() {
+        System.out.println("Welcome to");
+    }
+}
