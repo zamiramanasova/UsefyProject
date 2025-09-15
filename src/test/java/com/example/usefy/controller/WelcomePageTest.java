a4d45ec226ce7486ff51b6035574eeb1af8d791f
Zamira <keldibaevaz@gmail.com>
2025-09-15 21:16:24 +0300

    15/09/2025

--- src/test/java/com/example/usefy/controller/WelcomePageTest.java
+++ src/test/java/com/example/usefy/controller/WelcomePageTest.java
@@ -1,13 +1,6 @@
 package com.example.usefy.controller;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.ui.ModelMap;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class WelcomePageTest {
 
