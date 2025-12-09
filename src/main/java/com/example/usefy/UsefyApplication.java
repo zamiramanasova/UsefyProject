be1cfadac217ba7f3df8c08a5daa829bcd96770d
Zamira <keldibaevaz@gmail.com>
2025-12-09 23:14:43 +0300

    new feature

--- src/main/java/com/example/usefy/UsefyApplication.java
+++ src/main/java/com/example/usefy/UsefyApplication.java
@@ -1,15 +1,11 @@
-package com.example.usefy.controller;
+package com.example.usefy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.example.usefy.repository")
 public class UsefyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UsefyApplication.class, args);
     }
-
 }
