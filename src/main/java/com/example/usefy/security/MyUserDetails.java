44799cc82266d8af1b69240fa70bfe2159d84773
Zamira <keldibaevaz@gmail.com>
2025-12-09 23:57:03 +0300

    new feature

--- src/main/java/com/example/usefy/security/MyUserDetails.java
+++ src/main/java/com/example/usefy/security/MyUserDetails.java
@@ -1,4 +1,38 @@
 package com.example.usefy.security;
 
-public class MyUserDetails {
+import com.example.usefy.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+
+    private final User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(); // роли добавим позже
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
+
