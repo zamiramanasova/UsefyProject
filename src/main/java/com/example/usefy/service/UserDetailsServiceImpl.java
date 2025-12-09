bd9b1c7927ba0b4d311dd375b8d602dc70d60b16
Zamira <keldibaevaz@gmail.com>
2025-12-09 23:51:44 +0300

    new feature

--- src/main/java/com/example/usefy/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/usefy/service/UserDetailsServiceImpl.java
@@ -2,17 +2,27 @@ package com.example.usefy.service;
 
 import com.example.usefy.model.User;
 import com.example.usefy.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.usefy.security.MyUserDetails;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl {
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
 
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new MyUserDetails(user);
+    }
 }
+
