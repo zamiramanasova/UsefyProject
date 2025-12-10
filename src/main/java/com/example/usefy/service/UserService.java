b2592801869d9a40a09521d983c423b843b26c7d
Zamira <keldibaevaz@gmail.com>
2025-12-10 21:48:09 +0300

    new feature

---
+++ src/main/java/com/example/usefy/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.usefy.service;
+
+import com.example.usefy.model.User;
+
+public interface UserService {
+    User registerUser(String username, String password, String email);
+
+    User findByUsername(String username);
+}
---
+++ src/main/java/com/example/usefy/service/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.usefy.service;
+
+public class UserServiceImpl {
+}
