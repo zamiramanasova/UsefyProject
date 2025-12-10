f83e023f63bd94c82f1b8ce9bfdc7f6faf016d05
Zamira <keldibaevaz@gmail.com>
2025-12-10 21:37:02 +0300

    new feature

--- src/main/java/com/example/usefy/repository/UserRepository.java
+++ src/main/java/com/example/usefy/repository/UserRepository.java
@@ -2,12 +2,12 @@ package com.example.usefy.repository;
 
 import com.example.usefy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("Select u from User u where u.userName = :username")
-    public User getUserByUsername(@Param("username") String username);
+
+    Optional<User> findByUsername(String username);
 }
