208164e173aa1974ac6c34a16e60eda8d48c8165
Zamira <keldibaevaz@gmail.com>
2025-12-15 20:20:38 +0300

    added class Course.java

---
+++ src/main/java/com/example/usefy/model/Course.java
@@ -0,0 +1,23 @@
+package com.example.usefy.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "courses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
