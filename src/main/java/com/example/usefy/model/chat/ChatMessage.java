607cd4ec612dee25606be8323391d21603e62792
Zamira <keldibaevaz@gmail.com>
2026-01-31 22:11:30 +0300

    refactor dome classes and bags fix

--- src/main/java/com/example/usefy/model/chat/ChatMessage.java
+++ src/main/java/com/example/usefy/model/chat/ChatMessage.java
@@ -3,7 +3,6 @@ package com.example.usefy.model.chat;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-
 import java.time.LocalDateTime;
 
 @Entity
