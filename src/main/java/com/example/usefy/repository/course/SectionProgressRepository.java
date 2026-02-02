5b3e245c3a0278775f9653bcd70a128a000de89f
Zamira <keldibaevaz@gmail.com>
2026-02-02 20:38:45 +0300

    refactor dome classes and bags fix

--- src/main/java/com/example/usefy/repository/course/SectionProgressRepository.java
+++ src/main/java/com/example/usefy/repository/course/SectionProgressRepository.java
@@ -2,7 +2,6 @@ package com.example.usefy.repository.course;
 
 import com.example.usefy.model.course.SectionProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface SectionProgressRepository extends JpaRepository<SectionProgress, Long> {
