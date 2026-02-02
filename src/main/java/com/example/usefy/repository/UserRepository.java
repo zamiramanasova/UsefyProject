5b5bdf444f02a8ddcd94bd8ab9d4f3587938eb68
Zamira <keldibaevaz@gmail.com>
2026-02-02 20:40:31 +0300

    refactor dome classes and bags fix

--- src/main/java/com/example/usefy/repository/UserRepository.java
+++ src/main/java/com/example/usefy/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.usefy.repository;
 
 import com.example.usefy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
