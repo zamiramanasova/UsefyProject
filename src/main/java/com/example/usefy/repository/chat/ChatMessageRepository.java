44f7c353627acb0bd64e1d5c95706b3d260c8b81
Zamira <keldibaevaz@gmail.com>
2026-02-02 20:36:05 +0300

    refactor dome classes and bags fix

--- src/main/java/com/example/usefy/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/example/usefy/repository/chat/ChatMessageRepository.java
@@ -3,7 +3,6 @@ package com.example.usefy.repository.chat;
 import com.example.usefy.model.chat.ChatMessage;
 import com.example.usefy.model.chat.ChatSession;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
