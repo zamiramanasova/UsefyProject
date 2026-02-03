89a9a4a5c66c5ae05776d0badea84281aaf0cfb1
Zamira <keldibaevaz@gmail.com>
2026-02-03 22:30:05 +0300

    refactor dome classes and bags fix

--- src/main/java/com/example/usefy/service/UserService.java
+++ src/main/java/com/example/usefy/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.usefy.service;
 import com.example.usefy.model.User;
 
 public interface UserService {
+
     User registerUser(String username, String password, String email);
 
     User findByUsername(String username);
