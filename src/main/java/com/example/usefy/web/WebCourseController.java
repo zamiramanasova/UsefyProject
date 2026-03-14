0196c8730c9d1833fe7a442ab28b264fc1abc832
Zamira <keldibaevaz@gmail.com>
2026-03-14 15:57:29 +0600

    refactoring

--- src/main/java/com/example/usefy/web/WebCourseController.java
+++ src/main/java/com/example/usefy/web/WebCourseController.java
@@ -12,7 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;  // если нужно использовать статусы
+
 
 @Slf4j
 @Controller
